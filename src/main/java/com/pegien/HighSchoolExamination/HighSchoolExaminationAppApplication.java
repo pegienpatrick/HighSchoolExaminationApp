dca8803384e6bafa13f907916d51579505942e3f
patrick <ngupatgi@gmail.com>
2024-01-21 12:33:10 +0300

    First Project

---
+++ src/main/java/com/pegien/HighSchoolExamination/HighSchoolExaminationAppApplication.java
@@ -0,0 +1,13 @@
+package com.pegien.HighSchoolExamination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighSchoolExaminationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighSchoolExaminationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pegien/HighSchoolExamination/HighSchoolExaminationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pegien.HighSchoolExamination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighSchoolExaminationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
