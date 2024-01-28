425524fb3caa5c77718fff2e3ca5ca8bc10a6bd2
patrick <ngupatgi@gmail.com>
2024-01-28 16:59:48 +0300

    Patrick Update on Sun Jan 28 04:59:48 PM EAT 2024

---
+++ src/main/resources/static/assets/ssrBoot-jQU1c1QP.js
@@ -0,0 +1,1 @@
+import{y as s,b5 as t,z as e,s as n}from"./index-3uUFeXcg.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
