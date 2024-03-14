949c58f3b606ec93a1676f86f3aeef81524d4657
Patrick Githinji Ngunjiri <ngupatgi@gmail.com>
2024-03-14 05:45:46 +0300

    slots on Thu Mar 14 05:45:46 EAT 2024

---
+++ src/main/java/com/pegien/HighSchoolExamination/TimeTable/Slots/TimeTableSlot.java
@@ -0,0 +1,42 @@
+package com.pegien.HighSchoolExamination.TimeTable.Slots;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Time;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TimeTableSlot {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long num;
+
+    private SlotType slotType;
+
+
+    private Long Duration;
+
+    private int startHour;
+    private int startMinute;
+
+    private Long after;
+
+    @Column(length = 10000)
+    private String title;
+
+
+}
+
+
+enum SlotType{
+    BREAK,
+    LESSON
+}
---
+++ src/main/java/com/pegien/HighSchoolExamination/TimeTable/Slots/TimeTableSlotRepository.java
@@ -0,0 +1,6 @@
+package com.pegien.HighSchoolExamination.TimeTable.Slots;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TimeTableSlotRepository extends JpaRepository<TimeTableSlot,Long> {
+}
